b4e666a1cc2a86f5892c226bf90100ee56ee60ad
harshit-sharma <harshit.sharma@olacabs.com>
2017-04-03 17:11:50 +0530

    tensorflow tutorials

---
+++ tensor_math.py
@@ -0,0 +1,10 @@
+# Solution is available in the other "solution.py" tab
+import tensorflow as tf
+
+x = tf.placeholder(tf.string)
+y = tf.placeholder(tf.int32)
+z = tf.placeholder(tf.float32)
+
+with tf.Session() as sess:
+    output = sess.run(y, feed_dict={x: 'Test String', y: 123, z: 45.67})
+    print(output)
